22d7bbbe611d6c9ffb7acd50044566cc88e600fe
Turki <tbaz@windowslive.com>
2024-05-14 23:30:23 +0300

    last change

---
+++ api/community.js
@@ -0,0 +1,24 @@
+//routes
+const router = require("express").Router();
+const communityController = require("../controller/community");
+
+//Get list of all communities in the database
+router.get("/", communityController.getAllCommunities);
+
+// Add a new community to the database
+router.post("/newCommunity", communityController.createCommunity);
+
+// Get communities by name
+router.get("/searchCommunities", communityController.getCommunityByName);
+
+// Get a community by ID
+router.get("/", communityController.getCommunityById);
+
+// Update a community by ID
+router.put("/", communityController.updateCommunityById);
+
+// Delete a community by ID
+router.delete("/", communityController.deleteCommunityById);
+
+//export modules
+module.exports = router;
